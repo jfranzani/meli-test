1a992c4b69d16f624d81beb9c1490cc0c730baec
Jeronimo Franzani <jeronimofranzani@gmail.com>
2020-11-26 14:32:56 -0300

    add search-box component

---
+++ src/app/shared/search-box/search-box.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrls: ['./search-box.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SearchBoxComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+ 
